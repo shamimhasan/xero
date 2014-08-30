35ded3c7bb861d9c51632ca875bcd98f4e137ed5
Shamim <shamimhasan008@gmail.com>
2014-08-30 06:38:38 +0600

    key update

--- app/config/xero.php
+++ app/config/xero.php
@@ -1,8 +1,8 @@
 <?php
 
 return array(
-    'key' => 'XUYL6CQWNL0DLKRWLCOTQXRVWFWAFR',
-    'secret' => '7LPXQVF2ES6ABMDTISTOSAQVNN3VD3',
+    'key' => 'L99L0KDZJEAD9YP08IOUPHMAX0X48L',
+    'secret' => 'Y89R79BCZ0IHDYDVTGRGMRV7DRG4OK',
     'publicPath' => app_path() . '/config/xero/publickey.cer',
     'privatePath' => app_path() . '/config/xero/privatekey.pem',
     'format' => 'application/json'
